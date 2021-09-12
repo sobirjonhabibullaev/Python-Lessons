6bf62ee1ef2bc68864fbf4b9ae22780d9799c05d
Sobirjon Habibullaev <onlinecourses1999@gmail.com>
2021-09-12 17:09:34 +0900

    Son Topish O'yini

--- dars-25.py
+++ dars-25.py
@@ -4,40 +4,72 @@ DARS - 25 | Son Topish O'yini
 """
 import random as r
 
-# oylangan_son = 7
-# urinish = 0
+def tekshir_komp(oylangan_son, taxmin_son, urinish):
+    if oylangan_son != taxmin_son:
+        if oylangan_son > taxmin_son:
+            holat = "kattaroq"
+        else:
+            holat = "kichikroq"
+        return print(f"Xato! Men oylagan son bundan {holat}. Yana harakat qilib ko'ring.")
+    else:
+        print(f"TOPDINGIZ! {oylangan_son} sonini o'ylagan edim. {urinish} ta urunishda topdingiz. Tabriklayman!")
+        return 1
+        
+def yakuniy_natija(h_urinish, c_urinish):
+    if c_urinish < h_urinish:
+        print(f"Men {c_urinish} ta taxmin bilan topib, sizni yutdim!!!\n")
+    elif c_urinish > h_urinish:
+        print(f"Siz {h_urinish} ta taxmin bilan topdingiz va yutdingiz!!!\n")
+    else:
+        print(f"Durrang!  Ikkimiz ham {h_urinish} ta taxmin bilan topdik.")
 
-# print("Keling, o'ylangan sonni topish o'ynaymiz!")
-# taxmin_son = int(input("1 dan 10 gacha bir son o'yladim. Topa olasizmi?\n>>> "))
 
-# def tekshir_komp(oylangan_son, taxmin_son, *urinish):
-#     if oylangan_son != taxmin_son:
-#         if oylangan_son > taxmin_son:
-#             holat = "kattaroq"
-#         else:
-#             holat = "kichikroq"
-#         return print(f"Xato! Men oylagan son bundan {holat}. Yana harakat qilib ko'ring.")
-#     else:
-#         return print(f"TOPDINGIZ! {oylangan_son} sonini o'ylagan edim. {urinish} ta urunishda topdingiz. Tabriklayman!")
+A = True
 
+print("Keling, o'ylangan sonni topish o'ynaymiz!")
+
+while A:
+    oylangan_son = r.randint(1,10)
+    h_urinish, c_urinish = 0, 0
+    taxmin_son = int(input("1 dan 10 gacha bir son o'yladim. Topa olasizmi?\n>>> "))
+    while True:
+        h_urinish += 1
+        if tekshir_komp(oylangan_son, taxmin_son, h_urinish) == 1:
+            break
+        taxmin_son = int(input(""))
     
-print("1 dan 10 gacha son o'ylang. Men topishga harakat qilaman.")
-print("Son o'ylagan bo'lsangiz, istalgan tugmani bosing.")
+    print("1 dan 10 gacha son o'ylang. Men topishga harakat qilaman.")
+    start = input("Son o'ylagan bo'lsangiz, istalgan tugmani bosing.")
+    if start != "" or start != "\n":
+        aniqlash_xabari = "To'gri (T), men o'ylagan son bundan kattaroq (+), yoki kichikroq (-)?? "
+        c_son = r.randint(1, 10)
+        c_son_old = 1
+        c_son_max = 10
+        while True:
+            c_urinish += 1
+            ask = input(f"Siz {c_son} sonini o'yladingiz: " + aniqlash_xabari)
+            if c_son_old > c_son and c_son < c_son_max:
+                print("Meni aldamang")
+                break
+            if ask == "+":
+                c_son_old = c_son + 1
+                c_son = r.randint(c_son_old, c_son_max)
+            elif ask == "-":
+                c_son_max = c_son - 1
+                c_son = r.randint(c_son_old, c_son_max)
+            elif ask == "T" or ask == "t":
+                print(f"Soningizni {c_urinish} ta taxmin bilan topdim!")
+                break
 
-ask = ": To'g'ri (T), men o'ylgan son bundan kattaroq (+), yoki kichikroq (-)?\n>>> "
-def tekshir_odam(ask,*urinish):
-    print(f"Siz {urinish} sonini o'yladingiz{ask}")
+    yakuniy_natija(h_urinish, c_urinish)
 
-def taxmin_qil(*args):    
-    return r.randrange(1,11)
-print(taxmin_qil())
-
-# Chellengew Messege bor
-while True:
     yanami = int(input("Yana o'ynaymizmi: ha(1) / yo'q(0): "))
     if yanami == 1:
-        #code bor
-        continue
+        A = True
     else:
-        break
-        
+        A = False
+
+
+
+
+    
