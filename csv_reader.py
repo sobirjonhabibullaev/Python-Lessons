b83c4915da0d21d9b00952b5bb3a7c108f738806
Sobirjon Habibullaev <onlinecourses1999@gmail.com>
2021-09-19 18:03:18 +0900

    copying data in csv file

--- csv_reader.py
+++ csv_reader.py
@@ -1,41 +1,38 @@
 import csv
+import os
 import shutil
 
-file1 = open(
+file0 = open(
     "D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/balaclava_ski_mask.csv")
-file2 = open(
+file1 = open(
     "D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/eyeglasses.csv")
-file3 = open(
+file2 = open(
     "D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/face_no_mask.csv")
-file4 = open(
+file3 = open(
     "D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/face_other_covering.csv")
-file5 = open(
+file4 = open(
     "D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/face_shield.csv")
-file6 = open(
+file5 = open(
     "D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/face_with_mask.csv")
-file7 = open(
+file6 = open(
     "D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/face_with_mask_incorrect.csv")
-file8 = open(
+file7 = open(
     "D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/hijab_niqab.csv")
-
+file8 = open(
+    "D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/no_mask.csv")
 directory = 'D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/archive (1)/Medical mask/Medical mask/Medical Mask/images/'
-destination = 'D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/balaclava_ski_mask'
+destination = 'D:/Courses/Data Science Praktikum/Homeworks/Task-1/Datasets/Mask/no_mask/'
 
-files = [file1, file2, file3, file4, file5, file6, file7, file8]
-# for file in files[0]:
-#     csvreader = csv.reader(file)
-#     for row in csvreader:
-#         img = row[0]
-#         k = directory + img
-#         p = destination + img
-#         print(type(img))
-#         # shutil.move(directory, destination)
+def copy(directory, destination, src=''):
+    shutil.copy(directory+img, destination + img)
 
-for row in files[0]:
-    img = row
-    k = directory + img
-    p = destination + img
-    shutil.move(directory, destination)
+files = [file0, file1, file2, file3, file4, file5, file6, file7, file8]
+
+csvreader = csv.reader(files[8])
+for row in csvreader:
+    img = row[0]
+    copy("/path/directory","/path/destination", src=img)
+
 
 
 row.close()
